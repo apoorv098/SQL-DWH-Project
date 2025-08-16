3f2e6f07bdcb309c5e0406dceb68735b2724e517
apoorv098 <apoorv2001.99@gmail.com>
2025-08-16 18:55:36 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,16 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    the script sets up three schemas 
+    'bronze', 'silver', and 'gold'.
+
+*/
+
+
+CREATE SCHEMA IF NOT EXISTS bronze;
+CREATE SCHEMA IF NOT EXISTS silver;
+CREATE SCHEMA IF NOT EXISTS gold;
+
 
